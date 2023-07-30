4deae32b310782c9746ab2475761c3b7ef79a7dc
overwriter <1037152726@qq.com>
2023-07-30 17:17:44 +0800

    添加密钥

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 ﻿#include <QCoreApplication>
 #include "Qt-AES/qaesencryption.h"
 
+const QByteArray iv = QByteArray::fromStdString("0102030405060708");
+const QByteArray presetKey = QByteArray::fromStdString("0CoJUm6Qyw8W8jud");
+const QByteArray linuxapiKey = QByteArray::fromStdString("rFgB&h#%2?^eDg:Q");
+const QString base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const QString publicKey = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvaklV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44oncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----";
+const QString eapiKey = "e82ckenh8dichen8";
+
 /**
  * @brief encodeAES 使用AES算法对明文进行加密
  * @param plaintext 明文字符串
