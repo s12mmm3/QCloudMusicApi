9446cc249b8282fa377f29a602f40401f16764f5
overwriter <1037152726@qq.com>
2023-07-31 22:32:59 +0800

    替换加解密库为openssl

---
+++ crypto.hpp
@@ -0,0 +1,138 @@
+﻿#ifndef CRYPTO_H
+#define CRYPTO_H
+
+#include <QByteArray>
+#include <QObject>
+
+extern "C" {
+#include <openssl/rc4.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+}
+
+namespace Crypto {
+
+const QByteArray iv = QByteArray::fromStdString("0102030405060708");
+const QByteArray presetKey = QByteArray::fromStdString("0CoJUm6Qyw8W8jud");
+const QByteArray linuxapiKey = QByteArray::fromStdString("rFgB&h#%2?^eDg:Q");
+const QString base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const QString publicKey = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvaklV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44oncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----";
+const QString eapiKey = "e82ckenh8dichen8";
+
+/**
+ * @brief 使用AES-128-CBC算法加密数据的函数
+ * @param plainData 明文数据
+ * @param mode 加密算法
+ * @param key 加密密钥
+ * @param iv 偏移量
+ * @return QString 密文数据，如果加密失败则为空字符串
+ */
+QString aesEncrypt(const QString &plainData, const EVP_CIPHER *mode(), const QString &key, const QString &iv)
+{
+    // 创建加密上下文对象，并使用智能指针接管
+    QScopedPointer<EVP_CIPHER_CTX, QScopedPointerPodDeleter> ctx(EVP_CIPHER_CTX_new());
+    if (NULL == ctx.data())
+    {
+        return "";
+    }
+
+    // 将QString类型的输入参数转换为char*类型
+    QByteArray plainDataBytes = plainData.toUtf8();
+    QByteArray keyBytes = key.toUtf8();
+    QByteArray ivBytes = iv.toUtf8();
+    const char *plainDataChar = plainDataBytes.constData();
+    const char *keyChar = keyBytes.constData();
+    const char *ivChar = ivBytes.constData();
+
+    // 计算加密后的数据长度，至少要比明文数据长度大一个块大小
+    int outBufLen = plainDataBytes.size() + EVP_CIPHER_block_size(mode());
+    // 使用QByteArray代替动态分配的数组
+    QByteArray outBuf(outBufLen, '\0');
+
+    // 初始化加密上下文，设置加密算法、密钥、偏移量等参数
+    EVP_EncryptInit_ex(ctx.data(), mode(), NULL, (const unsigned char *)keyChar, (const unsigned char *)ivChar);
+    // 加密明文数据，将结果存放在outBuf中，更新outBufLen为输出长度
+    if (!EVP_EncryptUpdate(ctx.data(), (unsigned char *)outBuf.data(), &outBufLen, (const unsigned char *)plainDataChar, plainDataBytes.size()))
+    {
+        return "";
+    }
+
+    int tempLen = -1;
+    // 结束加密过程，处理剩余的数据，将结果追加到outBuf中，更新tempLen为输出长度
+    if (!EVP_EncryptFinal_ex(ctx.data(), (unsigned char *)outBuf.data() + outBufLen, &tempLen))
+    {
+        return "";
+    }
+
+    outBufLen += tempLen;
+
+    // 将outBuf中的密文数据转换为QByteArray类型，并使用Hex编码
+    QByteArray cipherDataBytes = outBuf.left(outBufLen).toHex();
+
+    // 将QByteArray类型的密文数据转换为QString类型
+    QString cipherData = QString::fromUtf8(cipherDataBytes);
+
+    return cipherData;
+}
+
+
+/**
+ * @brief 使用AES-128-CBC算法解密数据的函数
+ * @param cipherData 密文数据
+ * @param mode 解密算法
+ * @param key 解密密钥
+ * @param iv 偏移量
+ * @return QString 明文数据，如果解密失败则为空字符串
+ */
+QString aesDecrypt(const QString &cipherData, const EVP_CIPHER *mode(), const QString &key, const QString &iv)
+{
+    // 创建解密上下文对象，并使用智能指针接管
+    QScopedPointer<EVP_CIPHER_CTX, QScopedPointerPodDeleter> ctx(EVP_CIPHER_CTX_new());
+    if (NULL == ctx.data())
+    {
+        return "";
+    }
+
+    // 将QString类型的输入参数转换为char*类型，并使用Hex解码
+    QByteArray cipherDataBytes = QByteArray::fromHex(cipherData.toUtf8());
+    QByteArray keyBytes = key.toUtf8();
+    QByteArray ivBytes = iv.toUtf8();
+    const char *cipherDataChar = cipherDataBytes.constData();
+    const char *keyChar = keyBytes.constData();
+    const char *ivChar = ivBytes.constData();
+
+    // 计算解密后的数据长度，至少要和密文数据长度一样大
+    int outBufLen = cipherDataBytes.size();
+
+    // 使用QByteArray代替动态分配的数组
+    QByteArray outBuf(outBufLen, '\0');
+
+    // 初始化解密上下文，设置解密算法、密钥、偏移量等参数
+    EVP_DecryptInit_ex(ctx.data(), mode(), NULL, (const unsigned char *)keyChar, (const unsigned char *)ivChar);
+    // 解密密文数据，将结果存放在outBuf中，更新outBufLen为输出长度
+    if (!EVP_DecryptUpdate(ctx.data(), (unsigned char *)outBuf.data(), &outBufLen, (const unsigned char *)cipherDataChar, cipherDataBytes.size()))
+    {
+        return "";
+    }
+
+    int tempLen = -1;
+    // 结束解密过程，处理剩余的数据，将结果追加到outBuf中，更新tempLen为输出长度
+    EVP_DecryptFinal_ex(ctx.data(), (unsigned char *)outBuf.data() + outBufLen, &tempLen);
+    outBufLen += tempLen;
+
+    // 将outBuf中的明文数据转换为QByteArray类型，并使用UTF-8编码
+    QByteArray plainDataBytes = outBuf.left(outBufLen);
+
+    // 将QByteArray类型的明文数据转换为QString类型
+    QString plainData = QString::fromUtf8(plainDataBytes);
+
+    return plainData;
+}
+
+}
+
+
+
+#endif // CRYPTO_H
--- main.cpp
+++ main.cpp
@@ -1,58 +1,97 @@
 ﻿#include <QCoreApplication>
-#include "Qt-AES/qaesencryption.h"
+#include <QJsonDocument>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QTimer>
+#include "crypto.hpp"
 
-const QByteArray iv = QByteArray::fromStdString("0102030405060708");
-const QByteArray presetKey = QByteArray::fromStdString("0CoJUm6Qyw8W8jud");
-const QByteArray linuxapiKey = QByteArray::fromStdString("rFgB&h#%2?^eDg:Q");
-const QString base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const QString publicKey = "-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvaklV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44oncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----";
-const QString eapiKey = "e82ckenh8dichen8";
+extern "C" {
+#include <openssl/rc4.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+}
 
-/**
- * @brief encodeAES 使用AES算法对明文进行加密
- * @param plaintext 明文字符串
- * @param aesKey 密钥字符串
- * @param aesIv 偏移量字符串
- * @return 密文字符串，经过base64编码
- */
-QString encodeAES(QString plaintext, QByteArray presetKey, QByteArray iv)
-{
-    // 创建一个AES加密对象，指定AES_128模式，CBC模式和PKCS7填充方式
-    QAESEncryption aes(QAESEncryption::AES_128, QAESEncryption::CBC , QAESEncryption::PKCS7);
-    // 将明文、密钥和偏移量从QString转换为QByteArray
-    QByteArray input(plaintext.toLocal8Bit());
-    // 调用aes.encode方法对明文进行加密，并将结果转换为base64编码
-    QByteArray encodedText = aes.encode(input, presetKey, iv).toBase64();
-    // 将加密后的字节数组转换为QString并返回
-    QString encoded = encodedText;
-    QByteArray encoded1 = encoded.toUtf8();
-    return encoded1;
+QString getLrc(QString source) {
+    auto doc = QJsonDocument::fromJson(source.toUtf8());
+    return doc["lrc"]["lyric"].toString();
 }
 
 /**
- * @brief decodeAES 使用AES算法对密文进行解密
- * @param ciphertext 密文字符串，经过base64编码
- * @param aesKey 密钥字符串
- * @param aesIv 偏移量字符串
- * @return 明文字符串
+ * @brief 发送一个HTTP GET请求，并返回响应内容
+ * @param request 要发送的请求
+ * @return 如果请求成功，返回响应内容；如果请求失败或超时，返回空字节数组
  */
-QString decodeAES(QString ciphertext, QByteArray presetKey, QByteArray iv) {
-    // 创建一个AES解密对象，指定AES_128模式，CBC模式和PKCS7填充方式
-    QAESEncryption aes(QAESEncryption::AES_128, QAESEncryption::CBC , QAESEncryption::PKCS7);
-    // 将密文、密钥和偏移量从QString转换为QByteArray
-    QByteArray input(ciphertext.toLocal8Bit());
-    // 调用aes.decode方法对密文进行解密，并将结果从base64编码转换为原始字节数组
-    QByteArray decodedText = aes.decode(QByteArray::fromBase64(input), presetKey, iv);
-    // 调用aes.removePadding方法去除填充字符
-    decodedText = aes.removePadding(decodedText);
-    // 将解密后的字节数组转换为QString并返回
-    QString decrypted = decodedText;
-    QByteArray decrypted1 = decrypted.toUtf8();
-    return decrypted1;
+QByteArray get(QNetworkRequest request) {
+    QByteArray result; // 定义一个空字节数组作为结果
+    result.clear();
+    // 设置超时处理定时器
+    QTimer timer;
+    timer.setInterval(10000);  // 设置超时时间 10 秒
+    timer.setSingleShot(true);  // 单次触发
+
+    // 设置网络访问管理器
+    QNetworkAccessManager* manager = new QNetworkAccessManager();
+
+    // 开启一个局部的事件循环，等待响应结束，退出
+    QNetworkReply* reply = manager->get(request); // 发送GET请求
+    QEventLoop eventLoop;
+    QObject::connect(&timer, &QTimer::timeout, &eventLoop, &QEventLoop::quit); // 定时器超时时退出事件循环
+    QObject::connect(manager, &QNetworkAccessManager::finished, &eventLoop, &QEventLoop::quit); // 请求结束时退出事件循环
+    timer.start(); // 启动定时器
+    eventLoop.exec(); // 启动事件循环
+
+    if(timer.isActive()) { // 处理响应，定时器激活状态
+        timer.stop(); // 停止定时器
+        if(reply->error() != QNetworkReply::NoError) { // http请求出错，进行错误处理
+            qDebug() << "http请求出错 : " << reply->errorString();
+                                                     reply->deleteLater();
+            return result;
+        }
+        else {
+            // http - 响应状态码
+            int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+            qDebug() << "服务器返回的Code : " << statusCode;
+                if(statusCode == 200) { // http请求响应正常
+                result = reply->readAll(); // 读取响应内容
+                return result;
+            }
+            else {
+                reply->deleteLater();
+                return result;
+            }
+        }
+    }
+    else { // 超时处理
+        QObject::disconnect(manager, &QNetworkAccessManager::finished, &eventLoop, &QEventLoop::quit); // 断开连接
+        reply->abort(); // 中止请求
+        qDebug() << "http请求超时 ";
+        return result;
+    }
+    reply->deleteLater();
+    qDebug() << reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toString();
+    return result;
 }
 
 int main(int argc, char *argv[])
 {
-    auto ciphertext = encodeAES("HelloWorld!", presetKey, iv); // 使用AES加密算法加密字符串
-    qDebug() << ciphertext << decodeAES(ciphertext, presetKey, iv); // 打印密文和解密后的明文
+    QCoreApplication a(argc, argv);
+    auto buffer = QByteArray::fromStdString(
+        R"(/api/song/enhance/player/url/v1-36cd479b6b5-{"ids":"[33894312]","level":"exhigh","encodeType":"flac","header":{"appver":"8.10.05","versioncode":"140","buildver":"1690802117","resolution":"1920x1080","__csrf":"","os":"android","requestId":"1690802117125_0513","MUSIC_A":"bf8bfeabb1aa84f9c8c3906c04a04fb864322804c83f5d607e91a04eae463c9436bd1a17ec353cf780b396507a3f7464e8a60f4bbc019437993166e004087dd32d1490298caf655c2353e58daa0bc13cc7d5c198250968580b12c1b8817e3f5c807e650dd04abd3fb8130b7ae43fcc5b"}}-36cd479b6b5-ce3152d271ad1b582b16631d24cbcbf5)"
+        );
+    QString encrypt = Crypto::aesEncrypt(buffer, EVP_aes_128_ecb, Crypto::eapiKey, QString());
+    qDebug() << encrypt;
+    qDebug() << Crypto::aesDecrypt(encrypt, EVP_aes_128_ecb, Crypto::eapiKey, QString());
+
+    return a.exec();
 }
+//    QNetworkRequest request; // 创建一个网络请求对象
+//    QUrl qUrl("http://music.163.com/api/song/lyric?os=pc&id=2065967510&lv=-1&kv=-1&tv=-1"); // 创建一个URL对象
+//    request.setHeader(QNetworkRequest::UserAgentHeader,
+//                      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+//                      " Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.34"); // 设置请求头中的用户代理字段
+//    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json;charset=UTF-8"); // 设置请求头中的内容类型字段
+//    request.setUrl(qUrl); // 设置请求的URL
+//    qDebug() << get(request); // 调用get函数发送请求，并打印返回结果
