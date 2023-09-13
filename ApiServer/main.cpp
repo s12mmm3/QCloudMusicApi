bdd8ee2c9848c27f6883bade81f46874c264e2bd
s12mmm3 <1037152726@qq.com>
2023-09-13 23:47:03 +0800

    新增Http服务器，需要Qt6.4以上

---
+++ ApiServer/main.cpp
@@ -0,0 +1,64 @@
+﻿#include "../QCloudMusicApi/module.h"
+
+#include <QCoreApplication>
+#include <QHttpServer>
+#include <QJsonDocument>
+#include <QMetaMethod>
+#include <QMetaObject>
+#include <QUrlQuery>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    NeteaseCloudMusicApi api;
+
+    QHttpServer server;
+
+    auto parseRoute = [](QString funName) {
+        QString path = funName.replace("_", "/").trimmed();
+        return path;
+    };
+    for(int i = QObject().metaObject()->methodCount(); i < api.metaObject()->methodCount(); i++) {
+        auto funName = api.metaObject()->method(i).name();
+        auto path = "/" + parseRoute(funName);
+        server.route(
+            path,
+            QHttpServerRequest::Method::Get,
+            [=, &api](const QHttpServerRequest &request) {
+                QVariantMap query;
+
+                for(auto i: request.query().queryItems()) {
+                    query[i.first] = i.second;
+                }
+
+                QVariantMap args = {
+                    { "path", path },
+                    { "funName", funName },
+                    { "query", query }
+                };
+                qDebug().noquote() << QJsonDocument::fromVariant(args).toJson();
+
+                QByteArray ret;
+                bool ok = QMetaObject::invokeMethod(&api, funName
+                                                    , Qt::DirectConnection
+                                                    , Q_RETURN_ARG(QByteArray, ret)
+                                                    , Q_ARG(QVariantMap, query));
+                if(ok) {
+                    return ret;
+                }
+                else {
+                    return QByteArray();
+                }
+            });
+    }
+
+    quint16 port = 3000;
+    if(port == server.listen(QHostAddress::Any, port)) {
+
+    }
+    else {
+        qDebug() << "该端口号已被占用:" << port;
+    }
+    return a.exec();
+}
