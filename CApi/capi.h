50bafc7e7404785576d5c67b3162f20a25f8b795
overwriter <no@email.address>
2023-10-22 09:39:23 +0800

    feat:增加C语言风格动态库CApi，可供其他语言(Python、Java等)调用

---
+++ CApi/capi.h
@@ -0,0 +1,20 @@
+﻿#ifndef CAPI_H
+#define CAPI_H
+
+#if defined(CAPI_LIBRARY)
+#  define CAPI_EXPORT __declspec(dllexport)
+#else
+#  define CAPI_EXPORT __declspec(dllimport)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+CAPI_EXPORT char* invoke(char* funName, char* value);
+CAPI_EXPORT char* getFunName(int i);
+CAPI_EXPORT int getFunCount();
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CAPI_H
