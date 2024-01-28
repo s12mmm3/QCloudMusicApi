f38e079bf1228cb85868ae6137fd30f7c097da42
overwriter <no@email.address>
2024-01-28 12:09:46 +0800

    refactor:固定ua

--- QCloudMusicApi/util/request.h
+++ QCloudMusicApi/util/request.h
@@ -22,43 +22,20 @@
 namespace Request {
 
 QString chooseUserAgent(QString ua = "") {
-    const QVariantMap userAgentList {
+    const QMap<QString, QString> userAgentList {
         {
             QStringLiteral("mobile"),
-            QStringList {
-                // iOS 13.5.1 14.0 beta with safari
-                QStringLiteral(R"(Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1)"),
-                QStringLiteral(R"(Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.)"),
-                // iOS with qq micromsg
-                QStringLiteral(R"(Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML like Gecko) Mobile/14A456 QQ/6.5.7.408 V1_IPH_SQ_6.5.7_1_APP_A Pixel/750 Core/UIWebView NetType/4G Mem/103)"),
-                QStringLiteral(R"(Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.15(0x17000f27) NetType/WIFI Language/zh)"),
-                // Android -> Huawei Xiaomi
-                QStringLiteral(R"(Mozilla/5.0 (Linux; Android 9; PCT-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.64 HuaweiBrowser/10.0.3.311 Mobile Safari/537.36)"),
-                QStringLiteral(R"(Mozilla/5.0 (Linux; U; Android 9; zh-cn; Redmi Note 8 Build/PKQ1.190616.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/12.5.22)"),
-                // Android + qq micromsg
-                QStringLiteral(R"(Mozilla/5.0 (Linux; Android 10; YAL-AL00 Build/HUAWEIYAL-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2581 MMWEBSDK/200801 Mobile Safari/537.36 MMWEBID/3027 MicroMessenger/7.0.18.1740(0x27001235) Process/toolsmp WeChat/arm64 NetType/WIFI Language/zh_CN ABI/arm64)"),
-                QStringLiteral(R"(Mozilla/5.0 (Linux; U; Android 8.1.0; zh-cn; BKK-AL10 Build/HONORBKK-AL10) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/10.6 Mobile Safari/537.36)")
-            }
+            QStringLiteral("Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.1")
         },
         {
             QStringLiteral("pc"),
-            QStringList {
-                // macOS 10.15.6  Firefox / Chrome / Safari
-                QStringLiteral(R"(Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:80.0) Gecko/20100101 Firefox/80.0)"),
-                QStringLiteral(R"(Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.30 Safari/537.36)"),
-                QStringLiteral(R"(Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15)"),
-                // Windows 10 Firefox / Chrome / Edge
-                QStringLiteral(R"(Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0)"),
-                QStringLiteral(R"(Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.30 Safari/537.36)"),
-                QStringLiteral(R"(Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/13.10586)")
-                // Linux 就算了
-            }
+            QStringLiteral("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0")
         }
     };
-    auto list = userAgentList["mobile"].toStringList() + userAgentList["pc"].toStringList();
-    auto realUserAgentList = userAgentList.contains(ua) ? userAgentList[ua].toStringList() : list;
-    return QStringList {"mobile", "pc", ""}.indexOf(ua) > -1 ? realUserAgentList[QRandomGenerator::global()->generate() % realUserAgentList.size()]
-                                                            : ua;
+    if(ua == "mobile") {
+        return userAgentList["mobile"];
+    }
+    return userAgentList["pc"];
 }
 
 QVariantMap createRequest(QNetworkAccessManager::Operation method,
