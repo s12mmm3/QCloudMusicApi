17d09ddd11c9602f78b7cd1d137a610d39230478
overwriter <nobody@email.com>
2024-04-27 11:12:02 +0800

    refactor: 补充修改

--- QCloudMusicApi/util/config.h
+++ QCloudMusicApi/util/config.h
@@ -7,15 +7,15 @@ namespace QCloudMusicApiNS {
 namespace Config {
 
 static QString anonymous_token = QStringLiteral("de91e1f8119d32e01cc73efcb82c0a30c9137e8d4f88dbf5e3d7bf3f28998f21add2bc8204eeee5e56c0bbb8743574b46ca2c10c35dc172199bef9bf4d60ecdeab066bb4dc737d1c3324751bcc9aaf44c3061cd18d77b7a0");
-const static QMap<QString, QString> resourceTypeMap {
+const static QVariantMap resourceTypeMap {
     { "0", "R_SO_4_" },
     { "1", "R_MV_5_" },
     { "2", "A_PL_0_" },
     { "3", "R_AL_3_" },
     { "4", "A_DJ_1_" },
-    { "5", "R_VI_62_"},
+    { "5", "R_VI_62_" },
     { "6", "A_EV_2_" },
-    { "7", "A_DR_14_"}
+    { "7", "A_DR_14_" }
 };
 }
 }
--- QCloudMusicApi/util/index.h
+++ QCloudMusicApi/util/index.h
@@ -3,7 +3,6 @@
 
 #include "../global.h"
 
-#include <QNetworkCookie>
 #include <QVariantMap>
 
 namespace QCloudMusicApiNS {
@@ -11,9 +10,6 @@ class QCLOUDMUSICAPI_EXPORT Index {
 public:
     static QVariantMap stringToMap(const QString &cookie);
     static QString mapToString(const QVariantMap &cookie);
-    static QString cookieToString(const QList<QNetworkCookie> &cookie);
-    static QList<QNetworkCookie> stringToCookie(const QString &cookie);
-    static QList<QNetworkCookie> mapToCookie(QVariantMap &cookie);
     static QVariantMap mergeMap(const QVariantMap &map0, const QVariantMap &map1);
 };
 }
