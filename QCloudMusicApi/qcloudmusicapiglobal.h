a4800a1d9b67c3fac5baf16dd543af2ec86508a3
overwriter <9856mmm@gmail.com>
2025-04-22 21:47:42 +0800

    feat: 补充修改

--- QCloudMusicApi/qcloudmusicapiglobal.h
+++ QCloudMusicApi/qcloudmusicapiglobal.h
@@ -1,9 +1,16 @@
 #ifndef QCLOUDMUSICAPIGLOBAL_H
 #define QCLOUDMUSICAPIGLOBAL_H
 
-#include <QtCompilerDetection>
-
 #include "definevaluehelper.h"
+
+#if defined(_MSC_VER) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(WIN32) \
+|| defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define Q_DECL_EXPORT __declspec(dllexport)
+#define Q_DECL_IMPORT __declspec(dllimport)
+#else
+#define Q_DECL_EXPORT __attribute__((visibility("default")))
+#define Q_DECL_IMPORT __attribute__((visibility("default")))
+#endif
 
 #if defined(BUILD_SHARED_LIBS)
 #if defined(QCLOUDMUSICAPI_LIBRARY)
